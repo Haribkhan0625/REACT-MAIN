28d83860ff70198fcfca0f9a87e1be34fdcf02ad
Abdul Wahab Khan <haribkhan0625@gmail.com>
2024-10-22 14:51:25 +0500

    social media app completed

---
+++ Fragments/learning-fragments/src/App.jsx
@@ -0,0 +1,19 @@
+// import React from "react"
+import  "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+
+function App() {
+
+  return <>
+  <h1>Healthy Food</h1>
+  <ul class="list-group">
+  <li class="list-group-item">Banana</li>
+  <li class="list-group-item">Mango</li>
+  <li class="list-group-item">Veggies</li>
+  <li class="list-group-item">Fish</li>
+  <li class="list-group-item">Meat</li>
+</ul>
+</>
+}
+
+export default App
